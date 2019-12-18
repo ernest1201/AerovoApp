d89f15bc40a391d01bb9014e101f19b5d3afc57f
Daan Pouwels <18053181@student.hhs.nl>
2019-12-18 13:39:31 +0100

    Login, settings, create account, add devices

---
+++ app/src/main/java/com/example/myapplication/add_devices.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class add_devices : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_devices)
+    }
+}
